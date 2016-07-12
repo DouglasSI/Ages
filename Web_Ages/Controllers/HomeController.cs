e6b0d9c4d16d3c591880c62f00f29d19c946e7b8
Tr3mere <tr3mere@gmail.com>
2016-07-12 01:05:07 -0300

    ENCAIXADO

---
+++ Web_Ages/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Servico.Manter;
+namespace Web_Ages.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            tb_usuario user = new Manter_Usuario().obterUsuario("FABRICIO", "1234");
+            return View();
+        }
+
+    }
+}
